7ba8e4d72f36ab90779e81b1a844b2179323e133
Vitaliy Gukalov <gukalov.v@ukr.net>
2022-01-07 02:28:36 +0200

    basic app version ready

---
+++ src/components/FormAvatar/FormAvatar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FormAvatar.scss';
+
+const FormAvatar = () => {
+  return (
+    <img className='avatar' src='./img/form-avatar.png' alt='avatar' />
+  );
+}
+
+export default FormAvatar;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,6 +11,7 @@ import { ColorButton } from '../../components/Button/Button';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import FormAvatar from '../../components/FormAvatar/FormAvatar';
 
 const Login = () => {
   const [serverResult, setServerResult] = useState(null)
@@ -34,80 +35,83 @@ const Login = () => {
     >
       {(formikProps) => {
         return (
-          <Box className='form-container'>
-            <Form noValidate
-              className='form'
-              onSubmit={formikProps.handleSubmit}
-            >
-              <h2 className='form-title'>Member Login</h2>
-
-              <Field
-                component={CustomInput}
-                placeholder='Email'
-                name="email"
-                type="text"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon fontSize='small' />
-                    </InputAdornment>
-                  )
-                }}
-              />
-              <Field
-                data-testid="password"
-                component={CustomInput}
-                placeholder="Password"
-                name="password"
-                type="password"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockIcon fontSize='small' />
-                    </InputAdornment>
-                  )
-                }}
-              />
-
-              {serverResult && serverResult.error && (
-                <Box className='formStatusBlock'>
-                  <p className='error'>{serverResult.error}</p>
-                </Box>
-              )}
-
-              {serverResult && serverResult.success && (
-                <Box className='formStatusBlock'>
-                  <p className='success'>{serverResult.success}</p>
-                </Box>
-              )}
-
-              <ColorButton
-                variant="contained"
-                type='submit'
-                direction="form"
-                disabled={
-                  !formikProps.isValid ||
-                  formikProps.isSubmitting
-                }
+          <div className='page-positioning'>
+            <Box className='form-container'>
+              <FormAvatar />
+              <Form noValidate
+                className='form'
+                onSubmit={formikProps.handleSubmit}
               >
-                Login
-              </ColorButton>
+                <h2 className='form-title'>Member Login</h2>
 
-              <div className='login-options'>
-                <p className='user-info'>
-                  Forgot <Link to="#" className='form-link'>Username</Link>
-                  /
-                  <Link to="#" className='form-link'>Password</Link> ?
-                </p>
-                <p className='create-account'>
-                  <Link to="#" className='form-link flex'>
-                    Create your Account
-                    <ArrowRightAltIcon fontSize='small' />
-                  </Link>
-                </p>
-              </div>
-            </Form>
-          </Box>
+                <Field
+                  component={CustomInput}
+                  placeholder='Email'
+                  name="email"
+                  type="text"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailIcon fontSize='small' />
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <Field
+                  data-testid="password"
+                  component={CustomInput}
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockIcon fontSize='small' />
+                      </InputAdornment>
+                    )
+                  }}
+                />
+
+                {serverResult && serverResult.error && (
+                  <Box className='formStatusBlock'>
+                    <p className='error'>{serverResult.error}</p>
+                  </Box>
+                )}
+
+                {serverResult && serverResult.success && (
+                  <Box className='formStatusBlock'>
+                    <p className='success'>{serverResult.success}</p>
+                  </Box>
+                )}
+
+                <ColorButton
+                  variant="contained"
+                  type='submit'
+                  direction="form"
+                  disabled={
+                    !formikProps.isValid ||
+                    formikProps.isSubmitting
+                  }
+                >
+                  Login
+                </ColorButton>
+
+                <div className='login-options'>
+                  <p className='user-info'>
+                    Forgot <Link to="#" className='form-link'>Username</Link>
+                    /
+                    <Link to="#" className='form-link'>Password</Link> ?
+                  </p>
+                  <p className='create-account'>
+                    <Link to="#" className='form-link flex'>
+                      Create your Account
+                      <ArrowRightAltIcon fontSize='small' />
+                    </Link>
+                  </p>
+                </div>
+              </Form>
+            </Box>
+          </div>
         )
       }}
     </Formik>
